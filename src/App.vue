ed249fb75f0fc36de9a6ba14184053d9f38ddb51
jasminnasit <jasujnasit48@gmail.com>
2021-09-13 18:07:13 +0530

    created basic UI for tracker

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header title = "Task Tracker"/>
+    <Tasks @delete-task="deleteTask" :tasks="tasks"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+    tasks: []
+    }
+  },
+  methods:{
+    deleteTask(id){
+      if(confirm('Are you sure?')){
+        this.tasks = this.tasks.filter((task)=> task.id !== id )
+      }
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'School Appointment',
+        day: 'March 3rd at 1:30pm',
+        reminder: false,
+      }
+    ]
   }
 }
 </script>
@@ -22,5 +53,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.btn {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+
+.container{
+  padding: 20px;
 }
 </style>
